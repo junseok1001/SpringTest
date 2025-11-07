770ea70c308ba3785b8b1cf0c7a80a29011dbed0
ohjunseok(학원) <megaIT@megaIT>
2025-11-07 18:32:46 +0900

    [학원] - 연습문제1 완료

---
+++ src/main/java/com/sourjelly/springtest/ResponseRequestTest/Test01Controller.java
@@ -0,0 +1,26 @@
+package com.sourjelly.springtest.ResponseRequestTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class Test01Controller {
+
+
+    @ResponseBody
+    @RequestMapping("/lifecycle/test01/1")
+    public Map<String, Integer> Test01Response(){
+
+        Map<String, Integer> score = new HashMap<>();
+        score.put("국어", 80);
+        score.put("수학", 90);
+        score.put("영어", 85);
+
+        return score;
+    }
+
+}
