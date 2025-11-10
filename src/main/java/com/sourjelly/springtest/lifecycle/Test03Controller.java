373f85be763f4df669a5a975565dc0d2826b9cfd
ohjunseok(학원) <megaIT@megaIT>
2025-11-10 17:39:53 +0900

    [학원] - Thymeleaf bulid설정 추가 - Thymeleaf cacsh 설정 추가 - thymeleaf 설정 확인 테스트 추가

---
+++ src/main/java/com/sourjelly/springtest/lifecycle/Test03Controller.java
@@ -0,0 +1,15 @@
+package com.sourjelly.springtest.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03Controller {
+
+
+    @RequestMapping("/lifecycle/test03")
+    public String ThymeleafTest(){
+        return "/lifecycle/test03";
+    }
+
+}
