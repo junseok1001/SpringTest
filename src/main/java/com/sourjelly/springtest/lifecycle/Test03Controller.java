8cf903956b4c636f6b630c80b5a747b92dcd371b
ohjunseok(학원) <megaIT@megaIT>
2025-11-10 18:54:27 +0900

    [학원] - view thymeleaf 설정 추가

--- src/main/java/com/sourjelly/springtest/lifecycle/Test03Controller.java
+++ src/main/java/com/sourjelly/springtest/lifecycle/Test03Controller.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Test03Controller {
 
-
+    // html 경로를 통해 html 을 응답에 담는경우 ResponseBody 없음
     @RequestMapping("/lifecycle/test03")
     public String ThymeleafTest(){
+            // html 파일 내부 경로
+            // src/main/resources/templates 생략
+            // .html 생략
         return "/lifecycle/test03";
     }
 
