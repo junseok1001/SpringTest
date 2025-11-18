0b35c72c8dd31c07d6877670b613bf61bf441e72
ohjunseok(학원) <megaIT@megaIT>
2025-11-18 18:57:59 +0900

    [학원] - 11월 18일 수업내용 - thymeleaf 연습문제 완료 - 종합문제 1번 레이아웃 구성 완료

---
+++ src/main/java/com/sourjelly/springtest/thymeleaf/Member.java
@@ -0,0 +1,62 @@
+package com.sourjelly.springtest.thymeleaf;
+
+import java.time.LocalDate;
+
+public class Member {
+
+    private String name;
+    private LocalDate birth;
+    private String PhoneNumber;
+    private String email;
+    private String nationality;
+    private String introduce;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+}
